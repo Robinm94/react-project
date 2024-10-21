19e51968d1a803cd4981a9130e5f04ab08b95b0f
robinm2812 <robinm2812@gmail.com>
2024-10-20 23:56:10 -0400

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.VITE_APP_TMDB_TOKEN": JSON.stringify(
+      process.env.VITE_APP_TMDB_TOKEN,
+    ),
+  },
 });
